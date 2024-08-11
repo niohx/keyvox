59e802a0bb3fcd0c53b930e573b80bbb7a83feb0
niohx <nioh@live.jp>
2024-08-12 01:05:56 +0900

    change Lib

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='keyvox',
     version='0.1',
-    packages=find_packages()
+    packages=find_packages(where='Lib'),
+    package_dir={'': 'Lib'},
+    install_requires=[
+        'requests',
+    ],
 )
